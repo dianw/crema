e89364760bdef6a230873e60cc6e57c35c2e61ec
dianw <dianw.aditya@gmail.com>
2017-08-21 00:38:30 +0700

    Initialize CoreUI

---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="app">
+    <AppHeader/>
+    <div class="app-body">
+      <Sidebar :navItems="nav"/>
+      <main class="main">
+        <breadcrumb :list="list"/>
+        <div class="container-fluid">
+          <nuxt />
+        </div>
+      </main>
+      <!-- <AppAside/> -->
+    </div>
+    <AppFooter/>
+  </div>
+</template>
+
+<script>
+import { Header as AppHeader, Sidebar, Aside as AppAside, Footer as AppFooter, Breadcrumb } from '~/components/'
+
+export default {
+  name: 'full',
+  components: {
+    AppHeader,
+    Sidebar,
+    AppAside,
+    AppFooter,
+    Breadcrumb
+  },
+  data () {
+    return {
+      nav: [
+        {
+          name: 'Dashboard',
+          to: { name: 'index' },
+          icon: 'fa fa-dashboard'
+        }
+      ]
+    }
+  },
+  computed: {
+    name () {
+      return this.$route.name
+    },
+    list () {
+      return this.$route.matched
+    }
+  }
+}
+</script>
