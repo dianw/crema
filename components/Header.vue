0c53ab8735ef447f89847e1aa2eae6dafc5a4c10
dianw <dianw.aditya@gmail.com>
2017-08-23 23:57:28 +0700

    Add GitHub link

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,20 @@
 <template>
   <header class="app-header navbar">
-    <button class="navbar-toggler mobile-sidebar-toggler d-lg-none" type="button" @click="mobileSidebarToggle">&#9776;</button>
-    <b-link :to="{ name: 'index' }" class="navbar-brand">
-      <h2>crema</h2>
-    </b-link>
-    <button class="navbar-toggler sidebar-toggler d-md-down-none mr-auto" type="button" @click="sidebarMinimize">&#9776;</button>
-    <!-- <button class="navbar-toggler aside-menu-toggler d-md-down-none" type="button" @click="asideToggle">&#9776;</button> -->
+    <div class="container-fluid">
+      <button class="navbar-toggler mobile-sidebar-toggler d-lg-none" type="button" @click="mobileSidebarToggle">&#9776;</button>
+      <b-link :to="{ name: 'index' }" class="navbar-brand">
+        <h2>crema</h2>
+      </b-link>
+      <button class="navbar-toggler sidebar-toggler d-md-down-none mr-auto" type="button" @click="sidebarMinimize">&#9776;</button>
+      <ul class="nav navbar-nav">
+        <li class="nav-item">
+          <a class="btn btn-lg" href="https://github.com/dianw/crema" target="_blank">
+            <i class="fa fa-github fa-2"></i>
+          </a>
+        </li>
+      </ul>
+      <!-- <button class="navbar-toggler aside-menu-toggler d-md-down-none" type="button" @click="asideToggle">&#9776;</button> -->
+    </div>
   </header>
 </template>
 <script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'crema',
+    title: 'Crema - Graphical Crypto Toolkit',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -44,6 +44,12 @@ module.exports = {
   ** Build configuration
   */
   build: {
+    vendor: [
+      'node-forge',
+      'bootstrap-vue',
+      'vue-no-ssr',
+      '@nuxtjs/font-awesome'
+    ],
     /*
     ** Run ESLINT on save
     */
